8794fce0f88d62b3d6e089dc0fb4f6ad6aca1438
bhdrYildiz <bahadiryildiz137@gmail.com>
2023-02-05 03:26:36 +0300

    Controller updated

--- src/main/java/com/luv2code/springdemo/rest/CustomerRestController.java
+++ src/main/java/com/luv2code/springdemo/rest/CustomerRestController.java
@@ -3,8 +3,12 @@ package com.luv2code.springdemo.rest;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -35,6 +39,41 @@ public class CustomerRestController {
 		}
 		
 		return customer;
+	}
+	
+	@PostMapping("customers")
+	public Customer addCustomer(@RequestBody Customer customer) {
+		
+		customer.setId(0);
+		
+		customerService.saveCustomer(customer);
+		
+		return customer;	
+	}
+	
+	@PutMapping
+	public Customer updateCustomer(@RequestBody Customer customer) {
+		
+		customerService.saveCustomer(customer);
+		
+		return customer;
+	}
+	
+	@DeleteMapping
+	public String deleteCustomer(@PathVariable int customerId) {
+		
+		Customer tempCustomer = customerService.getCustomer(customerId);
+		
+		if(tempCustomer ==  null) {
+			
+			throw new CustomerNotFoundException("Customer id is not found! : "+customerId);
+			
+		}
+		
+		customerService.deleteCustomer(customerId);
+		
+		return "customer is deleted : " +customerId;
+		
 	}
 	
 }
